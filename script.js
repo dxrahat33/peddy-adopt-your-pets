64db0bdbf4e456e03b029486ebe82fd796108589
dxrahat33 <dxrahat33@gmail.com>
2024-10-11 17:29:19 +0600

    card done

--- script.js
+++ script.js
@@ -34,8 +34,9 @@ const displayCategories = (categories) => {
     const categoriesContainer = document.getElementById('categories-container')
     categories.forEach(item => {
         const btnDiv = document.createElement('div')
+        btnDiv.classList = ('flex items-center justify-center')
         btnDiv.innerHTML = `
-            <button class="flex items-center gap-4 px-12 py-4 border rounded-xl border-[rgba(14, 122, 129, 0.15)] cat-hover">
+            <button class="flex items-center gap-4 w-[180px] py-2 justify-center border rounded-xl border-[rgba(14, 122, 129, 0.15)] cat-hover">
                 <img src="${item.category_icon}"/>
                 <p class="text-2xl font-semibold">${item.category}</p>
             </button>
@@ -51,18 +52,18 @@ const displayAllPets = (pets) => {
         const petCard = document.createElement('div');
         petCard.classList = ('p-5 border rounded-xl')
         petCard.innerHTML = `
-            <img class="w-full max-h-[130px] rounded-xl" src="${pet.image}"/>
+            <img class="w-full md:max-h-[135px] lg:max-h-[130px] rounded-xl" src="${pet.image}"/>
             <h1 class="text-xl font-bold my-4">${pet.pet_name}</h1>
-            <div class="flex flex-col gap-2 justify-center">
-                <p class="flex gap-2"><img class="w-[25px] h-[25px] justify-center" src="https://img.icons8.com/?size=48&id=zukFPciIOcaq&format=png"/>Breed: ${getValidatedValue(pet, "breed")}</p>
+            <div class="flex flex-col gap-2 justify-center border-b pb-2">
+                <p class="flex gap-2 whitespace-nowrap"><img class="w-[25px] h-[25px] justify-center" src="https://img.icons8.com/?size=48&id=zukFPciIOcaq&format=png"/>Breed: ${getValidatedValue(pet, "breed")}</p>
                 <p class="flex gap-2"><img class="w-[25px]" src="https://img.icons8.com/?size=50&id=60611&format=png"/>Birth: ${getyearFromFullDate(pet.date_of_birth)}</p>
                 <p class="flex gap-2"><img class="w-[25px]" src="https://img.icons8.com/?size=50&id=1665&format=png"/>Gender: ${getValidatedValue(pet, "gender")}</p>
                 <p class="flex gap-2"><img class="w-[25px]" src="https://img.icons8.com/?size=50&id=2971&format=png"/>Price: ${validatePrice(pet, 'price')}</p>
             </div>
-            <div class="flex justify justify-between py-3 mt-4">
-                <button class="btn"><i class="fa-regular fa-heart"></i><button/>
-                <button class="btn p-1">Adopt<button/>
-                <button class="btn p-1">Details<button/>
+            <div class="flex py-3 mt-3 items-center justify-evenly">
+                <button class="btn btn-accent px-6 py-2 md:px-2"><i class="fa-regular fa-heart"></i><button/>
+                <button class="btn btn-accent px-6 py-2 md:px-2">Adopt<button/>
+                <button class="btn btn-accent px-5 py-2 md:px-2 ">Details<button/>
             <div/>
         `;
         petContainer.append(petCard);
